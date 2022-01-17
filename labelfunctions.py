dd797301e9d61e5703f20e6fe57dd4742cddd811
nvschilleman <nvschilleman@gmail.com>
2022-01-17 03:20:01 +0100

    Fixed issues

--- labelfunctions.py
+++ labelfunctions.py
@@ -25,26 +25,21 @@ class NetworkPrinter(object):
     
 class ZPLFunctions:
     def BuildLabel(self, listvar, qty):
-        OrderType, DeliveryNumber, BagQty, BoxQty, DryQty, CustName, CustStreet, CustZip, CustCity, CustPhone, Operator, Date, OrderNr = listvar
+        OrderType, DeliveryNumber, BoxQty, DryQty, CustName, CustStreet, CustZip, CustCity, CustPhone, Operator, Date, OrderNr = listvar
         self.zpl = ZPLDocument()
 
         #Alignment settings and shared label borders
         self.zpl.add_zpl_raw('^MMT^PW831^LL0440^LS0'
                              '^FO0,20^GFA,46592,46592,00104,:Z64:eJzszTERgDAUBbDPMTAiASmVBtKQggRGBq7FxRu4xEBqHgFXVUs8vSrRjHFMmedcMs+9Zp4n9WyZ5009LfP09lbA1tudeJbYs18ej8fj8Xg8Ho/H4/F4PB6Px+PxeDwej8fj8Xg8Ho/H4/F4PB6Px+PxeDwej8fj8Xg8Ho/H4/F4PB6Px+PxeDwej8fj8Xg8Ho/H4/F4PB6Px+PxeDwej8fj8Xg8Ho/H4/F4PB6Px+PxeDyePzwfAAAA///t1LENgCAARUGsLBnBUVjN0RiJ0sKgW/wQcm+B6x6Hw+FwOBwOh8PhcDgcDofD4XA4HA6Hw+FwOBwOh8PhcDgcDofD4XDWcNrYzPkixZwr47wpp2acJ+WcGWccGaeXjHOXlmBmYjrStv2w2OoE:11E9'
-                             '^FO496,377^GB329,0,5^FS^FO6,233^GB818,0,5^FS^FO496,305^GB329,0,5^FS^FO165,236^GB0,216,5^FS^FO491,236^GB0,216,5^FS')
+                             '^FO496,377^GB329,0,5^FS^FO6,233^GB818,0,5^FS^FO496,305^GB329,0,5^FS^FO247,236^GB0,216,5^FS^FO491,236^GB0,216,5^FS')
 
         #Order quantity data fields
-        self.zpl.add_field_origin(17, 290)
-        self.zpl.add_field_block(180,1,0,'C')
-        self.zpl.add_zpl_raw('^A0N,114,140')
-        self.zpl.add_field_data(BagQty) 
-
-        self.zpl.add_field_origin(180, 290)
+        self.zpl.add_field_origin(62, 290)
         self.zpl.add_field_block(180,1,0,'C')
         self.zpl.add_zpl_raw('^A0N,114,140')
         self.zpl.add_field_data(BoxQty) 
 
-        self.zpl.add_field_origin(342, 290)
+        self.zpl.add_field_origin(304, 290)
         self.zpl.add_field_block(180,1,0,'C')
         self.zpl.add_zpl_raw('^A0N,114,140')
         self.zpl.add_field_data(DryQty) 
@@ -59,14 +54,14 @@ class ZPLFunctions:
         self.zpl.add_zpl_raw('^A0N,30,40')
         self.zpl.add_field_data(OrderNr) 
         
-        self.zpl.add_field_origin(567, 388)
-        self.zpl.add_field_block(200,1,0,'C')
+        self.zpl.add_field_origin(547, 388)
+        self.zpl.add_field_block(230,1,0,'C')
         self.zpl.add_zpl_raw('^A0N,30,40')
         self.zpl.add_field_data(Date)        
         
         if OrderType == 'D':        
             #Row separators, labeltext
-            self.zpl.add_zpl_raw('^FO328,27^GB0,427,5^FS'                                 
+            self.zpl.add_zpl_raw('^FO328,27^GB0,208,5^FS'                                 
                                  '^FT544,441^A0N,24,36^FB231,1,0,C^FH^FDBEZORGDATUM^FS'
                                  '^FT31,223^A0N,24,36^FB269,1,0,C^FH^FDBESTEMMING NR.^FS'
                                  '^LRY^FO16,199^GB307,0,29^FS^LRN'
@@ -84,11 +79,11 @@ class ZPLFunctions:
             self.zpl.add_zpl_raw('^FT347,72^A0N,35,35^FH\^FD' + str(CustName) + '^FS'
                                  '^FT347,112^A0N,35,35^FH\^FD' + str(CustStreet) + '^FS'
                                  '^FT347,155^A0N,35,35^FH\^FD' + str(CustZip) + '^FS'
-                                 '^FT465,155^A0N,35,35^FH\^FD' + str(CustCity) + '^FS'
+                                 '^FT475,155^A0N,35,35^FH\^FD' + str(CustCity) + '^FS'
                                  '^FT374,215^A0N,35,35^FH\^FD' + str(CustPhone) + '^FS')
         elif OrderType == 'C':
-            #Row separators, labeltext
-            self.zpl.add_zpl_raw('^FO328,236^GB0,218,5^FS'
+            #Row separators, labeltext'^FO328,236^GB0,218,5^FS'
+            self.zpl.add_zpl_raw(
                                  '^FO6,193^GB818,0,5^FS'
                                  '^FT544,441^A0N,24,36^FB231,1,0,C^FH^FDAFHAALDATUM^FS')  
             self.zpl.add_field_origin(55, 45)
@@ -103,14 +98,13 @@ class ZPLFunctions:
 
         
         #Bottom Layer shared label data
-        self.zpl.add_zpl_raw('^FT25,441^A0N,24,36^FB125,1,0,C^FH^FDTASSEN^FS'
-                             '^FT198,441^A0N,24,36^FB101,1,0,C^FH^FDDOZEN^FS'
-                             '^FT358,441^A0N,24,36^FB108,1,0,C^FH^FDDROOG^FS'
+        self.zpl.add_zpl_raw('^FT80,441^A0N,24,36^FB101,1,0,C^FH^FDDOZEN^FS'
+                             '^FT321,441^A0N,24,36^FB108,1,0,C^FH^FDDROOG^FS'
                              '^FT536,295^A0N,24,36^FB247,1,0,C^FH^FDINGEPAKT DOOR^FS'
                              '^FT537,367^A0N,24,36^FB245,1,0,C^FH^FDORDERNUMMER^FS'
-                             '^LRY^FO16,417^GB144,0,29^FS^LRN'
-                             '^LRY^FO338,417^GB148,0,29^FS^LRN'
-                             '^LRY^FO175,417^GB148,0,29^FS^LRN'
+                             '^LRY^FO16,417^GB225,0,29^FS^LRN'
+
+                             '^LRY^FO258,417^GB228,0,29^FS^LRN'
                              '^LRY^FO500,417^GB318,0,29^FS^LRN'
                              '^LRY^FO500,271^GB318,0,29^FS^LRN'
                              '^LRY^FO500,343^GB318,0,29^FS^LRN')
